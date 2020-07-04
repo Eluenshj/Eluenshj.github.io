a84397011466c0799e2eef5fec0a8ab474a8d5fe
Eluneshj <shenghejun2009@163.com>
2020-07-04 23:18:39 +0800

    1

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "心爱的小可爱",  // 同上...
-        "今天是你的生日",
+        "2周年纪念日快乐",
    
     ],
     /**
@@ -17,8 +17,8 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "心爱的小可爱": "./imgs/1.JPG",
+      //   "2周年纪念日快乐": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
@@ -28,7 +28,7 @@ var config = {
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "二周年快乐",
         story: "A MESSAGE FOR YOU",
     }
 };
